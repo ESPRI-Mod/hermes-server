d3d755abf4e83e65dc4e9084f5ae845ea915f5da
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-14 00:31:55 +0200

    Updated db performance tests.

--- prodiguer_jobs/db/run_pgres_performance_tests.py
+++ prodiguer_jobs/db/run_pgres_performance_tests.py
@@ -1,167 +1,180 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: run_pgres_performance_tests.py
+   :copyright: Copyright "Apr 26, 2013", Institute Pierre Simon Laplace
+   :license: GPL/CeCIL
+   :platform: Unix
+   :synopsis: Executes prodiguer postgres database performance tests and writes results to file system.
+
+.. moduleauthor:: Mark Conway-Greenslade <momipsl@ipsl.jussieu.fr>
+
+
+"""
+import csv
+import os
 
 import arrow
 import psycopg2
 
 from prodiguer.db import pgres as db
 from prodiguer.utils import config
+from prodiguer.utils import logger
 
 
 
-class TestMetrics(object):
-	"""Encapsulates a set of test metrics.
+# Sql statement for selecting simulations.
+_SQL_SELECT_SIMULATIONS = """SELECT
+    s.*
+FROM
+    monitoring.tbl_simulation as s
+WHERE
+    s.execution_start_date IS NOT NULL AND
+    s.is_obsolete = false {};
+"""
 
-	"""
-	def __init__(self, level, desc, target):
-		self.data = None
-		self.desc = desc.upper()
-		self.level = level.upper()
-		self.target = target.upper()
-		self.time = None
-		self._then = None
+# Sql statement for selecting jobs.
+_SQL_SELECT_JOBS = """SELECT
+    j.*
+FROM
+    monitoring.tbl_job as j
+JOIN
+    monitoring.tbl_simulation as s ON j.simulation_uid = s.uid
+WHERE
+    s.execution_start_date IS NOT NULL AND
+    s.is_obsolete = false {};
+"""
 
-	def __repr__(self):
-		return "{}, {}, {}, {}, {}".format(self.time, self.level, self.target, self.desc, len(self.data))
+# Global now.
+_NOW = arrow.utcnow()
 
+# Set of timeslices to test.
+_TIMESLICES = [
+    "1W",
+    "2W",
+    "1M",
+    "2M",
+    "3M",
+    "6M",
+    "12M",
+    "ALL",
+]
 
-	def start(self):
-		self._then = arrow.now()
+# Map of timeslice tokens to time deltas.
+_TIMESLICE_DELTAS = {
+    '1W': _NOW.replace(days=-7),
+    '2W': _NOW.replace(days=-14),
+    '1M': _NOW.replace(days=-31),
+    '2M': _NOW.replace(days=-61),
+    '3M': _NOW.replace(days=-92),
+    '6M': _NOW.replace(days=-183),
+    '12M': _NOW.replace(days=-365),
+    'ALL': None,
+}
 
-	def end(self):
-		self.time = arrow.now() - self._then
+# Set of db drivers to test.
+_DRIVERS = [
+    "sqlalchemy",
+    "psycopg2"
+]
 
+# Set of db query targets.
+_TARGETS = [
+    "simulations",
+    "jobs"
+]
 
-class TestContextInformation(object):
-	"""Encapsulates testing contextual information.
+# Map of db query targets to sqlalchmey based functions.
+_SQLALCHEMY_FACTORIES = {
+    "simulations": db.dao_monitoring.retrieve_active_simulations,
+    "jobs": db.dao_monitoring.retrieve_active_jobs,
+}
 
-	"""
-	def __init__(self):
-		self.hl_01_sims = TestMetrics("high", "all", "sims")
-		self.hl_01_jobs = TestMetrics("high", "all", "jobs")
-		self.hl_02_sims = TestMetrics("high", "active timeslice", "sims")
-		self.hl_02_jobs = TestMetrics("high", "active timeslice", "jobs")
-		self.ll_01_sims = TestMetrics("low", "all", "sims")
-		self.ll_01_jobs = TestMetrics("low", "all", "jobs")
-		self.ll_02_sims = TestMetrics("low", "active timeslice", "sims")
-		self.ll_02_jobs = TestMetrics("low", "active timeslice", "jobs")
-		self.tests = [
-			self.hl_01_sims,
-			self.hl_01_jobs,
-			self.hl_02_sims,
-			self.hl_02_jobs,
-			self.ll_01_sims,
-			self.ll_01_jobs,
-			self.ll_02_sims,
-			self.ll_02_jobs,
-		]
-
-
-def get_psycopg2_connection():
-	"""Returns a low-level connection to remote database.
-
-	"""
-	try:
-	    return psycopg2.connect(config.db.pgres.main)
-	except:
-		raise RuntimeError("unable to connect to the database")
-
-
-def _do_test_hl_01(ctx):
-	db.session.start()
-
-	ctx.hl_01_sims.start()
-	ctx.hl_01_sims.data = db.dao.get_all(db.types.Simulation)
-	ctx.hl_01_sims.end()
-
-	ctx.hl_01_jobs.start()
-	ctx.hl_01_jobs.data = db.dao.get_all(db.types.Job)
-	ctx.hl_01_jobs.end()
-
-	db.session.end()
-
-
-def _do_test_hl_02(ctx):
-	db.session.start()
-
-	ctx.hl_02_sims.start()
-	ctx.hl_02_sims.data = db.dao_monitoring.retrieve_active_simulations()
-	ctx.hl_02_sims.end()
-
-	ctx.hl_02_jobs.start()
-	ctx.hl_02_jobs.data = db.dao_monitoring.retrieve_active_jobs()
-	ctx.hl_02_jobs.end()
-
-	db.session.end()
+# Map of db query targets to psycopg2 sql statements.
+_PSYCOPG2_FACTORIES = {
+    "simulations": _SQL_SELECT_SIMULATIONS,
+    "jobs": _SQL_SELECT_JOBS,
+}
 
 
-def _do_test_ll_01(ctx):
-	conn = get_psycopg2_connection()
-	cur = conn.cursor()
+def exec_sqlalchmey(timeslice_delta, target):
+    """Performs a sqlalchemy based db query."""
+    if timeslice_delta:
+        timeslice_delta = timeslice_delta.datetime
+    timeslice_factory = _SQLALCHEMY_FACTORIES[target]
+    db.session.start()
+    data = timeslice_factory(timeslice_delta)
+    db.session.end()
 
-	ctx.ll_01_sims.start()
-	cur.execute("""SELECT * from monitoring.tbl_simulation""")
-	ctx.ll_01_sims.data = cur.fetchall()
-	ctx.ll_01_sims.end()
-
-	ctx.ll_01_jobs.start()
-	cur.execute("""SELECT * from monitoring.tbl_job""")
-	ctx.ll_01_jobs.data = cur.fetchall()
-	ctx.ll_01_jobs.end()
-
-	conn.close()
+    return data
 
 
-def _do_test_ll_02(ctx):
-	conn = get_psycopg2_connection()
-	cur = conn.cursor()
+def exec_psycopg2(timeslice_delta, target):
+    """Performs a psycopg2 based db query."""
+    if timeslice_delta:
+        timeslice_delta = "AND \n\ts.execution_start_date >= '{}'".format(timeslice_delta)
+    else:
+        timeslice_delta = ""
+    conn = psycopg2.connect(config.db.pgres.main)
+    cur = conn.cursor()
+    sql = _PSYCOPG2_FACTORIES[target].format(timeslice_delta)
+    cur.execute(sql)
+    data = cur.fetchall()
+    conn.close()
 
-	ctx.ll_02_sims.start()
-	cur.execute("""	SELECT
-						s.*
-					FROM
-						monitoring.tbl_simulation as s
-				""")
-	ctx.ll_02_sims.data = cur.fetchall()
-	ctx.ll_02_sims.end()
-
-	ctx.ll_02_jobs.start()
-	cur.execute("""	SELECT
-					    j.simulation_uid,
-					    j.job_uid,
-					    j.accounting_project,
-					    j.execution_end_date,
-					    j.execution_start_date,
-						j.id,
-					    j.is_error,
-					    j.typeof,
-					    j.warning_delay,
-					    j.is_startup,
-					    j.post_processing_name,
-					    j.post_processing_date,
-					    j.post_processing_dimension,
-					    j.post_processing_component,
-					    j.post_processing_file,
-					    j.row_create_date,
-					    j.row_update_date
-				   	FROM
-				   		monitoring.tbl_job as j
-		           	JOIN
-		           		monitoring.tbl_simulation ON j.simulation_uid = monitoring.tbl_simulation.uid
-		           	WHERE
-		           		monitoring.tbl_simulation.execution_start_date IS NOT NULL AND
-		           		monitoring.tbl_simulation.is_obsolete = false
-		        """)
-	ctx.ll_02_jobs.data = cur.fetchall()
-	ctx.ll_02_jobs.end()
-
-	conn.close()
-
-# Run tests.
-ctx = TestContextInformation()
-for func in [_do_test_hl_01, _do_test_hl_02, _do_test_ll_01, _do_test_ll_02]:
-	func(ctx)
+    return data
 
 
-for t in ctx.tests:
-	print t
+# Map of db drivers to functions that perform queries.
+_DATA_FACTORIES = {
+    "sqlalchemy": exec_sqlalchmey,
+    "psycopg2": exec_psycopg2
+}
+
+# Set of CSV file headers.
+_CSV_HEADERS = ("TARGET", "TIMESLICE", "DB DRIVER", "RETURNED ROW COUNT", "QUERY TIME")
 
 
+def _get_metric(target, timeslice, driver):
+    """Returns a db performance metric.
+
+    """
+    timeslice_delta = _TIMESLICE_DELTAS[timeslice]
+    data_factory = _DATA_FACTORIES[driver]
+    then = arrow.now()
+    data = data_factory(timeslice_delta, target)
+    time = arrow.now() - then
+
+    return target, timeslice, driver, len(data), time
+
+
+def _get_metrics():
+    """Returns a collection of db performance metrics.
+
+    """
+    metrics = []
+    for target in _TARGETS:
+        for timeslice in _TIMESLICES:
+            for driver in _DRIVERS:
+                metrics.append(_get_metric(target, timeslice, driver))
+
+    return metrics
+
+
+def _main():
+    """Main entry point.
+
+    """
+    fname = "{}_server_pgres_performance_metrics_{}.csv".format(
+        os.getenv("PRODIGUER_MACHINE_TYPE"), _NOW.format('YYYY-MM-DD'))
+    fpath = os.path.join(os.getenv("PRODIGUER_HOME"), "tmp")
+    fpath = os.path.join(fpath, fname)
+    with open(fpath, 'wb') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(_CSV_HEADERS)
+        writer.writerows(_get_metrics())
+    logger.log_db("metrics written to --> {}".format(fpath))
+
+
+if __name__ == '__main__':
+    _main()
